9553b45a76e4028ba57b4799a5854530179e2311
Odeta <obudryte@gmail.com>
2019-10-24 20:40:54 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './assets/style.scss'
+//nuotrakas cia importuoti nes jei neimportuosi istrins.
+// import './assets/img/logo@1X.png';
+import './assets/img/isometricPc.png';
+import './assets/img/timezonePc.png';
+import './assets/img/viroMediaPc.png';
+import './assets/img/blogPc.png';
+import './assets/img/blueCircle.png';
+import './assets/img/purpleCircle.png';
+import './assets/img/greenCircle.png';
+import './assets/img/orangeCircle.png';
+import './assets/img/fbIcon.png';
+import './assets/img/twitterIcon.png';
+import './assets/img/linkedInIcon.png';
+import './assets/img/mailIcon.png';
+import './assets/img/flagIcon.png';
+import './assets/img/crayonIcon.png';
+import './assets/img/gearsIcon.png';
+import './assets/img/rocketIcon.png';
+import './assets/img/logo.png';
